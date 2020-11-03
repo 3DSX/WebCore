3fd293f71a7015671bfe5fc0d27fb35c4a7d4697
Alberto <admin@darksnakegames.com>
2020-11-03 20:17:54 +0100

    Controlador Asignatura

---
+++ Controllers/AsignaturaController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebCore.Models;
+
+namespace WebCore.Controllers
+{
+    public class AsignaturaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var asignatura = new AsignaturaModel
+            {
+                Nombre = "Física o Química"
+            };
+
+            ViewBag.Fecha = DateTime.Now;
+
+            return View(asignatura);
+
+        }
+
+        public IActionResult MultiAsignatura()
+        {
+
+            List<AsignaturaModel> asignaturas = new List<AsignaturaModel>
+            {
+                    new AsignaturaModel() {Nombre = "Francés"},
+                    new AsignaturaModel() {Nombre = "Español"},
+                    new AsignaturaModel() {Nombre = "Inglés"},
+                    new AsignaturaModel() {Nombre = "Química"},
+                    new AsignaturaModel() {Nombre = "Matemáticas"},
+                    new AsignaturaModel() {Nombre = "E.F"},
+                    new AsignaturaModel() {Nombre = "Programación"}
+            };
+
+            return View(asignaturas);
+        }
+    }
+}
