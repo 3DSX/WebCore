77f0c845b70030dd5b91150fe693bb09fcfffb6e
Alberto <admin@darksnakegames.com>
2020-11-03 20:20:10 +0100

    GUID Alumno y Vistas de Alumno

--- Models/AlumnoModel.cs
+++ Models/AlumnoModel.cs
@@ -1,11 +1,14 @@
 ﻿
 using System.Collections.Generic;
+using System;
 
 namespace WebCore.Models
 {
     public class AlumnoModel : ParentEntityModel
     {
         public List<EvaluacionModel> Evaluaciones { get; set; }
+
+        public AlumnoModel() => UniqueId = Guid.NewGuid().ToString();
 
     }
 }
