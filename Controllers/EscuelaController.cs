9caa08e70eb9e34937f99dc6fff87c075de63b28
Alberto <admin@darksnakegames.com>
2020-11-08 05:04:39 +0100

    Refactoring EscuelaController

--- Controllers/EscuelaController.cs
+++ Controllers/EscuelaController.cs
@@ -1,26 +1,23 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using WebCore.Models;
 
 namespace WebCore.Controllers
 {
-    public class EscuelaController : Controller
+    public class EscuelaController : ParentController
     {
         public IActionResult Index()
         {
-            var escuela = new EscuelaModel
-            {
-                YearOfCreation = 1998,
-                UniqueId = Guid.NewGuid().ToString(),
-                Nombre = "Academia Kadic",
-                Ciudad = "París",
-                TipoEscuela = TiposEscuelaModel.Bachillerato,
-                Direccion = "La Mezquita",
-                Pais = "Francia"
-            };
+            ViewBag.halloween = "La Monja";
 
-            ViewBag.halloween = "Voces";
-            return View(escuela);
+            return View(_context.Escuelas.FirstOrDefault());
         }
+
+        public EscuelaController(EscuelaContext context) : base(context)
+        {
+
+        }
+
     }
 }
