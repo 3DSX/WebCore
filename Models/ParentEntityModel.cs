70f24d08b471080a0a3a49160db1d6650a31025e
Alberto <admin@darksnakegames.com>
2020-11-08 05:05:05 +0100

    Set Primary Key (convention)

--- Models/ParentEntityModel.cs
+++ Models/ParentEntityModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebCore.Models
 {
     public abstract class ParentEntityModel
     {
+        [Key]
         public string UniqueId { get; set; }
+
         public string Nombre { get; set; }
 
         public override string ToString()
